36c12ef4acff440851c2cfaa17f727d66d37d65a
easywget <145293642+easywget@users.noreply.github.com>
2023-09-18 05:38:38 +0800

    Update minecraft_java.sh
    v.1.02
    change ram setting and time

--- minecraft_java.sh
+++ minecraft_java.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# Pull the Docker image
 docker pull itzg/minecraft-server
-docker run -d -e EULA=TRUE --name minecraft -p 25565:25565 --restart=always itzg/minecraft-server
+
+# Start the Minecraft server in a Docker container
+docker run -d \
+  -e EULA=TRUE \
+  -e JVM_DD_OPTS="-Duser.timezone=Asia/Singapore" \
+  --name minecraft \
+  -p 25565:25565 \
+  -e MEMORY=4G \
+  --restart=always \
+  itzg/minecraft-server
+
+
+# Optionally, you can add more instructions or commands here.
+
+# End of the script
