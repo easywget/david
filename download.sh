6be3d60daefb587d78fc3e9bee738f1460001c0a
easywget <145293642+easywget@users.noreply.github.com>
2023-09-18 04:50:23 +0800

    Update download.sh
    version 1.05

--- download.sh
+++ download.sh
@@ -1,15 +1,14 @@
 # Create the wget.sh script
-
 #!/bin/bash
 
 timedatectl set-timezone Asia/Singapore
 apt update && apt upgrade -y
 
 file_urls=(
-"https://github.com/easywget/david/blob/main/portainer.sh"
-"https://github.com/easywget/david/blob/main/filebrowser.sh"
-"https://github.com/easywget/david/blob/main/minecraft_java.sh"
-"https://github.com/easywget/david/blob/main/minecraft_bedrock.sh"
+"https://raw.githubusercontent.com/easywget/david/main/portainer.sh"
+"https://raw.githubusercontent.com/easywget/david/main/filebrowser.sh"
+"https://raw.githubusercontent.com/easywget/david/main/minecraft_java.sh"
+"https://raw.githubusercontent.com/easywget/david/main/minecraft_bedrock.sh"
 )
 for url in "${file_urls[@]}"; do
     wget "$url"
