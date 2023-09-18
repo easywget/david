e184b06ca54b73bdb9ff3d5b52e81763ff6f241f
easywget <145293642+easywget@users.noreply.github.com>
2023-09-18 17:18:20 +0800

    Update minecraft_java.sh

--- minecraft_java.sh
+++ minecraft_java.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-# Pull the Docker image
 docker pull itzg/minecraft-server
-
-# Start the Minecraft server in a Docker container
-docker run -d \
-  -e EULA=TRUE \
-  -e JVM_DD_OPTS="-Duser.timezone=Asia/Singapore" \
-  --name minecraft \
-  -p 25565:25565 \
-  -e MEMORY=4G \
-  --restart=always \
-  itzg/minecraft-server
-
-
-# Optionally, you can add more instructions or commands here.
-
-# End of the script
+docker run -d -e EULA=TRUE -p 25565:25565 --name minecraft-java --restart=always -e MEMORY=4G \
