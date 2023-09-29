5126f23c713914cd11f6c6719c38a1f7f0f2918d
easywget <145293642+easywget@users.noreply.github.com>
2023-09-29 09:06:23 +0800

    Update AdGuardHome.sh

--- AdGuardHome.sh
+++ AdGuardHome.sh
@@ -2,7 +2,7 @@
 
 # Update and upgrade the system
 apt-get install curl -y
-apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Install AdGuard Home
 curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
