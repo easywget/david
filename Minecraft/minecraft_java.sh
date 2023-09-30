c14532e076311dbfb9bf4ae6115aecd04607ce0f
easywget <145293642+easywget@users.noreply.github.com>
2023-09-30 08:44:28 +0800

    Update minecraft_java.sh

--- Minecraft/minecraft_java.sh
+++ Minecraft/minecraft_java.sh
@@ -2,3 +2,5 @@
 
 docker pull itzg/minecraft-server
 docker run -d -e EULA=TRUE -p 25565:25565 --name minecraft-java --restart=always -e MEMORY=4G itzg/minecraft-server
+docker run -d -e EULA=TRUE -p 25565:25565 --name minecraft-java --restart=always -e MEMORY=4G --tty --interactive itzg/minecraft-server
+
