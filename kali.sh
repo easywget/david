6189a70383ffcd112f118a98010c991850aa3689
easywget <145293642+easywget@users.noreply.github.com>
2023-10-03 08:22:29 +0800

    Create kali.sh

---
+++ kali.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt full-upgrade
+
+
+sudo apt-get install curl -y
+sudo apt-get install fail2ban -y
+sudo apt-get install vsftpd -y
+sudo apt-get install masscan -y
+sudo apt-get install cupp -y
+sudo apt-get install geoip-bin -y
+sudo apt-get install dsniff -y
+sudo apt-get install apache2 -y
+sudo apt-get install whois -y
+sudo apt-get install dos2unix -y
+sudo apt-get install geany -y
+sudo apt-get install wget -y
+sudo apt-get install etherape -y
+sudo apt-get install hydra -y
+sudo apt-get install nmap -y
+sudo apt-get install vsftpd -y
