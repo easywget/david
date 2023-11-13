ed6fa42b3eee45cdf33efedfb3de594be0c3e16b
easywget <145293642+easywget@users.noreply.github.com>
2023-11-13 19:27:41 +0800

    Create cfc.sh

---
+++ cfc.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install rsyslog -y
+sudo apt-get install vsftpd -y
+sudo apt-get install geoip-bin -y
+sudo apt-get install tor -y 
+sudo apt-get install geany -y
+pip install wget
+sudo msfdb init
