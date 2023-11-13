ff62079ca491e14168178d327955ca22d7ce7fc8
easywget <145293642+easywget@users.noreply.github.com>
2023-11-13 19:38:47 +0800

    Update cfc.sh

--- cfc.sh
+++ cfc.sh
@@ -4,5 +4,6 @@ sudo apt-get install vsftpd -y
 sudo apt-get install geoip-bin -y
 sudo apt-get install tor -y 
 sudo apt-get install geany -y
+sudo apt-get install cherrytree -y
 pip install wget
 sudo msfdb init
