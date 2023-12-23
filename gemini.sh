fc0ac35fb4db1dc377887aa0122f73e5ccfd2726
easywget <145293642+easywget@users.noreply.github.com>
2023-12-23 12:46:30 +0800

    Create gemini.sh

---
+++ gemini.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#check if python3 is installed
+python3 --version
+
+#install python3
+apt update
+apt install python3 -y
+
+#install pip
+apt install python3-pip -y
+
+pip install streamlit
+pip install python-dotenv
+pip google-generativeai
