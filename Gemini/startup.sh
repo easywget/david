14ed6ae087e1c4899c98e970ae7db4225439add8
easywget <145293642+easywget@users.noreply.github.com>
2023-12-31 08:07:36 +0800

    Add files via upload

---
+++ Gemini/startup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define variables
+service_unit_file="/etc/systemd/system/gemini-streamlit.service"
+current_username="$(whoami)"
+streamlit_command="/usr/local/bin/streamlit"
+app_script="/opt/gemini/app.py"
+
+# Create the systemd service unit file
+cat <<EOF | sudo tee "$service_unit_file" > /dev/null
+[Unit]
+Description=Streamlit Gemini Application
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=$streamlit_command run $app_script
+Restart=on-failure
+User=$current_username
+WorkingDirectory=$(dirname $app_script)
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd manager configuration
+sudo systemctl daemon-reload
+
+# Enable and start the service
+sudo systemctl enable gemini-streamlit.service
+sudo systemctl start gemini-streamlit.service
