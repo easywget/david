f7d5af9cac268c7c2b390081b5b4fc3b71a6e231
easywget <145293642+easywget@users.noreply.github.com>
2024-01-16 19:51:47 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,8 +4,14 @@ apt install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+
+
 docker pull portainer/portainer-ce:latest
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-docker run -d --name filebrowser --restart unless-stopped -p 8080:80 -v /:/srv filebrowser/filebrowser
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -e TZ=Asia/Singapore portainer/portainer-ce:latest
+
+docker pull filebrowser/filebrowser
+
+docker run -d --name filebrowser --restart unless-stopped -p 8080:80 -v /:/srv -e TZ=Asia/Singapore filebrowser/filebrowser
