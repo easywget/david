cc489bd377abbcb526307b6ad8345518402dc687
easywget <145293642+easywget@users.noreply.github.com>
2024-01-16 19:31:01 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,5 +7,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 docker pull portainer/portainer-ce:latest
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -e TZ=$(timedatectl show --property=Timezone --value) -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-docker run -d --name filebrowser --restart unless-stopped -e TZ=Asia/Singapore -p 8080:80 -v /:/srv filebrowser/filebrowser
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d --name filebrowser --restart unless-stopped -p 8080:80 -v /:/srv filebrowser/filebrowser
